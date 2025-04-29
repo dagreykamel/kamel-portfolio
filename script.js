705e98313441d37b0ef50284c441ccc27766e593
dagreykamel <149790308+dagreykamel@users.noreply.github.com>
2025-04-30 00:01:28 +0100

    script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
     let lastScrollY = window.scrollY;
 const footer = document.getElementById('footer');
 
-window.addEventListener('scroll', () => {
-            if (window.scrollY < lastScrollY) {
-                // ki ttl3 ywrilk footer
-                footer.classList.add('show');
-            } else {
-                // ki thwd ywrilk footer
-                footer.classList.remove('show');
-            }
-            lastScrollY = window.scrollY;
-});
-            </script>;
+window.addEventListener('scroll', function() {
+    const footer = document.queryselector('.footer');
+    if ((window.scrollY + window.innerheight) >= document.body.offsetheight) {
+
+        footer.classList.add('show');
+    } else {
+        
+        footer.classList.remove('show');
+    }
+
+}); < /script>;
