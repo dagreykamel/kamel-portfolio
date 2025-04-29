ee1e5ee86dc433cf5f188c19688955e8fbe89fb9
dagreykamel <149790308+dagreykamel@users.noreply.github.com>
2025-04-29 22:35:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+< script >
+    let lastScrollY = window.scrollY;
+const footer = document.getElementById('footer');
+
+window.addEventListener('scroll', () => {
+            if (window.scrollY < lastScrollY) {
+                // ki ttl3 ywrilk footer
+                footer.classList.add('show');
+            } else {
+                // ki thwd ywrilk footer
+                footer.classList.remove('show');
+            }
+            lastScrollY = window.scrollY;
+
+            <
+            /script>;
